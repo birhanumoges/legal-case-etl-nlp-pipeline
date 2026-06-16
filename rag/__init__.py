fe2d3997093dc4a8fcb2837fe1357338f8254d51
birhanumoges <birhanumoges726@gmail.com>
2026-06-16 14:30:46 -0300

    add new file: rag/__init__.py

---
+++ rag/__init__.py
@@ -0,0 +1,6 @@
+from .retriever     import LegalRetriever
+from .generator     import LegalGenerator
+from .rag_pipeline  import RAGPipeline
+from .query_engine  import QueryEngine
+
+__all__ = ["LegalRetriever","LegalGenerator","RAGPipeline","QueryEngine"]
