199063c8db3528718ade6ab214fbc923667a2e2e
birhanumoges <birhanumoges726@gmail.com>
2026-06-16 14:18:39 -0300

    add new file: timeseries/__init__.py

---
+++ timeseries/__init__.py
@@ -0,0 +1,5 @@
+from .trend_analysis    import TrendAnalyzer
+from .yearly_statistics import YearlyStatistics
+from .forecasting       import LegalForecaster
+
+__all__ = ["TrendAnalyzer","YearlyStatistics","LegalForecaster"]
